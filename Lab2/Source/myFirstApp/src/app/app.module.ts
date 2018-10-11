4f859d5ee7382f99f64e7f99feef9c8c885c836b
Anvesh <anveshmandadi821@gmail.com>
2018-10-11 11:10:45 -0500

    lab2

--- Lab2/Source/myFirstApp/src/app/app.module.ts
+++ Lab2/Source/myFirstApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import {QRScanner} from '@ionic-native/qr-scanner';
+import { Toast } from '@ionic-native/toast';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -15,6 +16,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
 import{RegisterPage} from '../pages/register/register';
 import { LocalNotifications } from '@ionic-native/local-notifications';
+import { DataServiceProvider } from '../providers/data-service/data-service';
 
 @NgModule({
   declarations: [
@@ -46,7 +48,9 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     LocalNotifications,
-    QRScanner
+    QRScanner,
+    DataServiceProvider,
+    Toast
   ]
 })
 export class AppModule {}
--- Lab2/Source/myFirstApp/src/pages/about/about.ts
+++ Lab2/Source/myFirstApp/src/pages/about/about.ts
@@ -1,35 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner'
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Toast } from '@ionic-native/toast';
+import { DataServiceProvider } from '../../providers/data-service/data-service';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController,private qrScanner: QRScanner) {
+  products: any = [];
+  selectedProduct: any;
+  productFound:boolean = false;
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner, private toast: Toast, public dataService: DataServiceProvider) {
+    this.dataService.getListDetails()
+      .subscribe((response)=> {
+        this.products = response;
+        console.log("at barcoed constructor");
+        console.log(this.products);
+      });
   }
-  clickCamera()
-  {
-    this.qrScanner.prepare()
-      .then((status: QRScannerStatus) => {
-        if (status.authorized) {
-          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-            console.log('Scanned something', text);
-            this.qrScanner.hide();
-            scanSub.unsubscribe();
-          });
-
-        } else if (status.denied) {
-          // camera permission was permanently denied
-          // you must use QRScanner.openSettings() method to guide the user to the settings page
-          // then they can grant the permission from there
-        } else {
-          // permission was denied, but not permanently. You can ask for permission again at a later time.
+  clickCamera(){
+    this.selectedProduct = {};
+    this.barcodeScanner.scan().then((barcodeData) => {
+      console.log("scanned product");
+      console.log(barcodeData.text);
+      this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
+      if(this.selectedProduct !== undefined) {
+        this.productFound = true;
+      } else {
+        this.productFound = false;
+        this.toast.show(`Product not found`, '5000', 'center').subscribe(
+          toast => {
+            console.log(toast);
+          }
+        );
+      }
+    }, (err) => {
+      this.toast.show(err, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
         }
-      })
-      .catch((e: any) => console.log('Error is', e));
+      );
+    });
   }
-
 }
